1a4188fb13ebafdd0a753215666f63a163a8da8b
DBStirling <davidstirling17@gmail.com>
2024-09-22 18:02:12 -0400

    correctly parses string for num1, operator and num2

--- script.js
+++ script.js
@@ -15,15 +15,64 @@ btns.forEach(btn => {
 // clear button functionality
 const clear = document.querySelector('#clear')
 
-clear.addEventListener('clear', () => {
+clear.addEventListener('click', () => {
     calcString = '';
+    console.log(calcString)
 })
 
 
+// equal button functionality
+
+const equals = document.querySelector('#equals')
+
+equal.addEventListener('click', () => {
+    runCalculation(calcString);
+})
+
+function runCalculation(string) {
+
+    operatorIndex = getOperator(string);
+
+    num1 = string.slice(0, operatorIndex);
+    operator = string[operatorIndex];
+    num2 = string.slice(operatorIndex+1);
+
+    console.log(num1)
+    console.log(operator)
+    console.log(num2)
 
 
 
+    // let answer = calculate(num1, operator, num2);
+};
 
+function getOperator(string) {
+
+    let operatorIndex;
+    for (let i = 0; i <string.length; i++) {
+        if (string[i] == '+' || string[i] == '-' || string[i] == 'x' || string[i] == '/')
+        operatorIndex = i;
+    }
+    console.log(operatorIndex);
+    if (operatorIndex == undefined) {
+        console.log('Cannot calculate. No operator!')
+        calcString = '';
+        return;
+    }
+    return operatorIndex;
+}
+
+// function calculate(num1, operator, num2) {
+//     if (operator == '+') return add(num1, num2);
+//     else if (operator == '-') return subtract(num1, num2);
+//     else if (operator == 'x') return multiply(num1, num2);
+//     else if (operator == '/') return divide(num1, num2);
+//     else {
+//         console.log("operator not recognized");
+//         calcString = ''
+//         return;
+//     }
+// }
 
 
 
@@ -31,16 +80,16 @@ clear.addEventListener('clear', () => {
 
 function add(a, b) {
     return a + b;
-}
+};
 
 function subtract(a,b) {
     return a - b;
-}
+};
 
 function multiply(a,b) {
     return a*b;
-}
+};
 
 function divide(a,b) {
     return a/b;
-}
+};
